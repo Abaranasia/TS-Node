136b7d20b69a9d515f52e57f04b0ad185c673539
Ran Kirlian <rankirlian@gmail.com>
2022-04-30 00:06:38 +0200

    Add Hero class & Power Interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import { Hero as Superhero } from "./classes/Hero";
+
+// import * as HeroClasses from "./classes/Hero"; 
+// We're creating an alias for the complete file and then we can use then independly in this way:
+// HeroClasses.hero
 
 
-console.log('Hola Mundo!');
+const ironman= new Superhero('Ironman',  1, 35) // Superhero is an alias, but it references to the class Hero at all levels
 
-
-
+console.log(ironman.power);
 
 
 
